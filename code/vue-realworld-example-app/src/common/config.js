e384473d06517e491485ee4a28a49753cca6506b
Gilad Sharaby <gsharaby@infinipoint.io>
2020-10-29 10:33:06 +0200

    updated config

--- code/vue-realworld-example-app/src/common/config.js
+++ code/vue-realworld-example-app/src/common/config.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://a3665d566d02f4731b07df7dfa074dfc-763543653.eu-west-1.elb.amazonaws.com/api";
+export const API_URL = "http://af3e874f5885e4834ae9819e9694d3d6-777939446.eu-west-1.elb.amazonaws.com/api";
 export default API_URL;
